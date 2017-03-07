a74d1e7103732caf4b00755788aa5d41cc69d1a9
leichaofan <leichaofan2024@gmail.com>
2017-03-07 21:40:50 +0800

    add post button

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "groups#index"
-  resources :groups 
+  resources :groups do
+    resources :posts
+  end  
   get "test" => "welcome#test"
   get "about", to: "welcome#about"
 end
