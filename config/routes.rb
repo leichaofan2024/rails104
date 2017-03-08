c1981932a1dded675fe7e320c95d6b2413e8f8f8
leichaofan <leichaofan2024@gmail.com>
2017-03-08 13:55:33 +0800

    create my groups

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,16 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "groups#index"
+  namespace :account do
+    resources :groups
+    resources :posts 
+  end
+
   resources :groups do
     member do
       post :join
       post :quit
-    end 
+    end
     resources :posts
   end
   get "test" => "welcome#test"
