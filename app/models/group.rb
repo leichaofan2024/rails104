b6d8f7ef85391f9c54b9eef9b54cae9d792ed3f4
leichaofan <leichaofan2024@gmail.com>
2017-03-08 08:37:54 +0800

    implement group relation

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,5 @@ class Group < ApplicationRecord
   validates :description, presence: true
   belongs_to :user
   has_many :posts
+  has_many :members, through: :group_relationships, source: :user 
 end
